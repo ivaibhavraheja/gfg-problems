3a4a700c53d224881014505830fb57f9ac8811e0
Vaibhav Raheja <112800995+ivaibhavraheja@users.noreply.github.com>
2022-11-28 18:23:48 +0530

    Update kth smallest element in BST.cpp

--- kth smallest element in BST.cpp
+++ kth smallest element in BST.cpp
@@ -7,16 +7,21 @@ class Solution {
                 return -1;
             }
             
+            //search in left Subtree
             int left = solve(root->left, K, i);
+            
+            //if element is found in leftSubtree then return 
             if(left != -1){
                 return left;
             }
             i++;
             
+            // if current element is k'th smallest, return it
             if(i == K){
                 return root->data;
             }
             
+            //else search in right Subtree
             return solve(root->right, K, i);
         }
     // Return the Kth smallest element in the given BST
